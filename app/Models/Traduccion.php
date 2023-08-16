a47ab31e3de2612919e4d701237bc8525b282480
fabrisdev <ezepuntocom@hotmail.com>
2023-08-16 20:02:32 -0300

    Creación de modelo para traducir

---
+++ app/Models/Traduccion.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Traduccion extends Model
+{
+    use HasFactory;
+    protected $table = "traducciones";
+}
